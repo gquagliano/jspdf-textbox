d01bbaac826efd8e55ee92e23eec28b63dabd83e
Gabriel <gabriel.quagliano@gmail.com>
2023-09-13 02:06:13 -0300

    Implemented new tokens and options. Misc. bug fixes.

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 import { trim } from "lodash";
 
 /**
- * @param {jsPDF} doc 
- */
-function textMaxWidth(doc, text, maxWidth) {
-    //TODO
-    //this one will measure by char, breaking words
-}
-
-/**
  * Parses a string and returns a list of instructions (tokens) to draw the text in the document.
  * @param {string} text - Input text.
  * @returns {object[]}
  */
 function parseFormattedText(text) {
-    //Just **bold** for now
+    //**bold**
+    //# h1
+    //## h2
 
     let buffer = "",
         open = {
+            h1: false,
+            h2: false,
             bold: false
         },
+        ignoreWhitespaces = false,
         stack = [];
 
     text = text.replace(/(\r\n|\n\r)/g, "\n");
@@ -37,27 +34,126 @@ function parseFormattedText(text) {
             continue;
         }
 
+        if(!buffer && text[i] == "#") {
+            if(text[i + 1] == "#") {
+                open.h2 = true;
+                stack.push({ h2: true });
+                i++; //skip next #
+            } else {
+                open.h1 = true;
+                stack.push({ h1: true });
+            }
+
+            ignoreWhitespaces = true;
+            continue;
+        }
+
         if(text[i] == "\n") {
             if(buffer)
                 stack.push({ text: buffer });
             buffer = "";
             stack.push({ br: true });
+
+            if(open.h1 || open.h2)
+                stack.push({ p: true });
+
+            open.h1 = false;
+            open.h2 = false;
+            ignoreWhitespaces = false;
+
             continue;
         }
 
         if(text[i] == " ") {
-            stack.push({ text: buffer + " " });
+            if(ignoreWhitespaces)
+                continue;
+
+            if(buffer)
+                stack.push({ text: buffer });
             buffer = "";
+            stack.push({ space: true });
             continue;
         }
 
         buffer += text[i];
+
+        if(text[i] != " ")
+            ignoreWhitespaces = false;
     }
 
     if(buffer)
         stack.push({ text: buffer });
 
     return stack;
+}
+
+/**
+ * Updates the tokens horizontal positioning, if necessary, to justify one line of text.
+ * @param {object[]} tokens - List of tokens to update.
+ * @param {number} startX - Start X value.
+ * @param {number} lineWidth - Line width.
+ * @param {string} textAlign - `left`, `right`, `center`, `justify`, `justify-right` or `justify-center`.
+ * @param {boolean} br - Wether the line ended because of an implicit line break, or, if `false` an overflow.
+ */
+function justifyLine(doc, tokens, startX, maxWidth, textAlign, br) {
+    if(!tokens.length)
+        return;
+
+    let lastToken,
+        textWidth = 0,
+        lineWidth = 0,
+        wordCount = 0;
+    for(let i = 0; i < tokens.length; i++) {
+        if(tokens[i].text) {
+            lastToken = tokens[i];
+            wordCount++;
+            textWidth += tokens[i].width;
+        }
+    }
+
+    if(!lastToken)
+        return;
+
+    lineWidth = lastToken.x + lastToken.width - startX;
+
+    if(lineWidth >= maxWidth)
+        return;
+
+    if(br) {
+        if(textAlign == "justify-right")
+            textAlign = "right";
+        else if(textAlign == "justify-center")
+            textAlign = "center";
+        else if(textAlign == "justify")
+            textAlign = "left";
+    }
+
+    if(textAlign == "right") {
+        let add = maxWidth - lineWidth;
+        for(let token of tokens)
+            if(token.text)
+                token.x += add;
+        return;
+    }
+
+    if(textAlign == "center") {
+        let add = (maxWidth - lineWidth) / 2;
+        for(let token of tokens)
+            if(token.text)
+                token.x += add;
+        return;
+    }
+
+    if(textAlign == "justify" || textAlign == "justify-right" || textAlign == "justify-center") {
+        let spaceWidth = (maxWidth - textWidth) / (wordCount - 2),
+            x = startX;
+        for(let i = 0; i < tokens.length; i++)
+            if(tokens[i].text) {
+                tokens[i].x = x;
+                x += tokens[i].width + spaceWidth;
+            }
+        return;
+    }
 }
 
 /**
@@ -74,182 +170,304 @@ function parseFormattedText(text) {
  * @param {boolean} [options.ellipsis] - Wether or not to add ellipsis if the text overflows the box.
  * @param {boolean} [options.lineBreak] - If `false`, lines won't break unless a `\n` is found. Default value is `true`.
  * @param {boolean} [options.pageBreak] - Wether or not to add new pages if necessary. Default value is `true`.
- * @param {boolean} [options.wordBreak] - Wether or not to break words. Default value is `false`.
- * @param {string} [options.textAlign] - Text align, as `"left"` (default), `"right"` or `"center"`.
+ * @param {string} [options.textAlign] - Text align, as `"left"` (default), `"right"`, `"center"`, `"justify"`, `"justify-right"` or `"justify-center"`.
+ * @param {object} [styles] - Styles.
  * @constructs {TextBox}
  */
-export function textBox(doc, text, { startY = 0, margin, width, baseline = "top", numLines, maxHeight, ellipsis, lineBreak = true, pageBreak = true, wordBreak, textAlign = "left" }) {
+export function textBox(doc, text, { startY = 0, margin, width, baseline = "top", numLines, maxHeight, ellipsis, lineBreak = true, pageBreak = true, wordBreak, textAlign = "left", styles = {} }) {
     this.data = [];
     this.pages = 1;
     this.finalY = 0;
     this.finalHeight = 0;
 
-    //TODO text align, break words
-    //TODO columns
-    //TODO justify text
-    //TODO implement word break, if false, break anyways if not even one word can be fit in the total width
-
-    const pageW = doc.internal.pageSize.getWidth(),
-        pageH = doc.internal.pageSize.getHeight();
-
-    if(typeof margin == "number")
-        margin = {
-            left: margin,
-            top: margin,
-            bottom: margin,
-            right: margin
-        };
-
-    margin = {
-        left: 0,
-        top: 0,
-        bottom: 0,
-        right: 0,
-        ...margin
+    let defaultStyle = {
+        fontSize: doc.getFontSize(),
+        bold: false
     };
 
-    if(!startY)
-        startY = margin.top;
+    this.compile = function () {
+        const pageW = doc.internal.pageSize.getWidth(),
+            pageH = doc.internal.pageSize.getHeight();
 
-    if(!width)
-        width = pageW - margin.left - margin.right;
+        if(typeof margin == "number")
+            margin = {
+                left: margin,
+                top: margin,
+                bottom: margin,
+                right: margin
+            };
 
-    if(!maxHeight)
-        maxHeight = pageH - margin.top - margin.bottom;
+        if(!startY)
+            startY = margin.top;
 
-    const maxY = margin.top + maxHeight;
+        if(!width)
+            width = pageW - (margin.left || 0) - (margin.right || 0);
 
-    //TODO validate margins, sizes, maxHeight, maxY (if maxHeight is present, it can't be larger than the available space)
+        if(!maxHeight)
+            maxHeight = pageH - (margin.top || 0) - (margin.bottom || 0);
 
-    let x = margin.left,
-        y = startY,
-        lines = 0,
-        lineHeight = 0,
-        lineHeightFactor = doc.getLineHeightFactor();
+        if(typeof margin.top == "undefined")
+            margin.top = startY;
 
-    let stack = parseFormattedText(text);
+        if(typeof margin.left == "undefined")
+            if(typeof margin.right == "number")
+                margin.left = pageW - margin.right - width;
+            else
+                margin.left = 0;
 
-    ///Measure only, this will give the caller the dimensions in order to draw something else below the text first
+        //TODO position box relative to the bottom of the page--this would require computing the actual height first
 
-    doc.saveGraphicsState();
+        const maxY = margin.top + maxHeight,
+            maxX = margin.left + width;
 
-    //start in the next page if there isn't enoguh space for at least one line
-    if(y + lineHeight > maxY) {
-        this.data.push({ pageBreak: true });
-        this.pages++;
-        y = margin.top;
+        //TODO validate margins, sizes, maxHeight, maxY (if maxHeight is present, it can't be larger than the available space)
+
+        let x = margin.left,
+            y = startY,
+            lines = 1,
+            lineHeight = computeCurrentLineHeight(defaultStyle);
+
+        let stack = parseFormattedText(text),
+            currentTextAign = textAlign, //in the future, there might be a token to set the next paragraph align
+            skipToNextLine,
+            currentStyle,
+            currentLineStart = 0;
+
+        doc.saveGraphicsState();
+
+        //start in the next page if there isn't enoguh space for at least one line
+        if(y + lineHeight > maxY) {
+            this.data.push({ pageBreak: true });
+            this.pages++;
+            y = margin.top;
+        }
+
+        for(let i = 0; i < stack.length; i++) {
+            let token = stack[i];
+
+            if(token.br) {
+                skipToNextLine = false;
+
+                x = margin.left;
+                y += (currentStyle?.marginBottom || 0) + lineHeight;
+                lines++;
+
+                getLineAndJustify(this.data, currentLineStart, currentTextAign, true);
+                currentLineStart = this.data.length;
+
+                if(y > maxY || (numLines && lines > numLines)) {
+                    if(pageBreak) {
+                        this.data.push({ pageBreak: true });
+                        this.pages++;
+                        y = margin.top;
+                        lines = 1;
+                    } else {
+                        break;
+                    }
+                }
+
+                continue;
+            }
+
+            if(token.h1) {
+                currentStyle = applyStyles(styles.h1);
+                lineHeight = computeCurrentLineHeight(currentStyle);
+                this.data.push(token);
+                continue;
+            }
+
+            if(token.h2) {
+                currentStyle = applyStyles(styles.h2);
+                lineHeight = computeCurrentLineHeight(currentStyle);
+                this.data.push(token);
+                continue;
+            }
+
+            if(token.p) {
+                currentStyle = applyStyles(); //revert to default
+                lineHeight = computeCurrentLineHeight(currentStyle);
+                this.data.push(token);
+                continue;
+            }
+
+            if(token.bold) {
+                currentStyle = applyStyles({ bold: true });
+                this.data.push(token);
+                continue;
+            }
+
+            if(token.bold === false) {
+                currentStyle = applyStyles({ bold: false });
+                this.data.push(token);
+                continue;
+            }
+
+            if(skipToNextLine)
+                continue;
+
+            if(token.space) {
+                let { w } = doc.getTextDimensions(" "); //the width can change in each iteration
+
+                x += w;
+
+                if(x >= maxX) {
+                    if(lineBreak) {
+                        x = margin.left;
+                        y += currentStyle?.marginBottom || lineHeight;
+                        lines++;
+                    } else {
+                        skipToNextLine = true;
+
+                        if(ellipsis)
+                            addEllipsis(this.data);
+                    }
+
+                    getLineAndJustify(this.data, currentLineStart, currentTextAign);
+                    currentLineStart = this.data.length;
+                }
+
+                if(y > maxY || (numLines && lines > numLines)) {
+                    if(pageBreak) {
+                        this.data.push({ pageBreak: true });
+                        this.pages++;
+                        x = margin.left;
+                        y = margin.top;
+                        lines = 1;
+                    } else {
+                        if(ellipsis)
+                            addEllipsis(this.data);
+
+                        break;
+                    }
+                }
+
+                continue;
+            }
+
+            if(token.text) {
+                let { w } = doc.getTextDimensions(token.text);
+
+                if(x + w > maxX) {
+                    if(lineBreak) {
+                        x = margin.left;
+                        y += currentStyle?.marginBottom || lineHeight;
+                        lines++;
+                    } else {
+                        skipToNextLine = true;
+
+                        if(ellipsis)
+                            addEllipsis(this.data);
+                    }
+
+                    getLineAndJustify(this.data, currentLineStart, currentTextAign);
+                    currentLineStart = this.data.length;
+
+                    if(!lineBreak)
+                        continue;
+                }
+
+                if(y > maxY || (numLines && lines > numLines)) {
+                    if(pageBreak) {
+                        this.data.push({ pageBreak: true });
+                        this.pages++;
+                        x = margin.left;
+                        y = margin.top;
+                        lines = 1;
+                    } else {
+                        if(ellipsis)
+                            addEllipsis(this.data);
+
+                        break;
+                    }
+                }
+
+                token.x = x;
+                token.y = y;
+                token.width = w;
+                this.data.push(token);
+
+                x += w;
+            }
+        }
+
+        getLineAndJustify(this.data, currentLineStart, currentTextAign, true);
+
+        this.finalY = y + lineHeight;
+        this.finalHeight = this.pages == 1
+            ? y - startY + lineHeight
+            : y - margin.top + lineHeight;
+
+        doc.restoreGraphicsState();
+    };
+
+    function computeCurrentLineHeight(style) {
+        let lineHeightFactor = style?.lineHeight || doc.getLineHeightFactor(),
+            { h: lineHeight } = doc.getTextDimensions("test");
+
+        return lineHeight * lineHeightFactor;
     }
 
-    let skipToNextLine;
+    function applyStyles(styles) {
+        if(typeof styles == "undefined")
+            styles = defaultStyle;
 
-    for(let token of stack) {
-        if(token.br) {
-            skipToNextLine = false;
+        doc.setFont(undefined, styles.bold ? "bold" : "normal");
 
-            x = margin.left;
-            y += lineHeight;
-            lines++;
+        if(styles.fontSize)
+            doc.setFontSize(styles.fontSize);
 
-            if(y > maxY || (numLines && lines > numLines)) {
-                if(pageBreak) {
-                    this.data.push({ pageBreak: true });
-                    this.pages++;
-                    y = margin.top;
-                    lines = 0;
-                } else {
-                    break;
-                }
-            }
+        return styles;
+    }
 
-            this.data.push(token);
-            continue;
-        }
+    function addEllipsis(data) {
+        if(!data.length)
+            return;
 
-        if(token.bold) {
-            doc.setFont(undefined, "bold");
-            this.data.push(token);
-            continue;
-        }
-
-        if(token.bold === false) {
-            doc.setFont(undefined, "normal");
-            this.data.push(token);
-            continue;
-        }
-
-        if(skipToNextLine)
-            continue;
-
-        if(token.text) {
-            let { w, h } = doc.getTextDimensions(token.text);
-
-            h *= lineHeightFactor;
-
-            if(h > lineHeight)
-                lineHeight = h;
-
-            if(x + w > width) {
-                if(lineBreak) {
-                    x = margin.left;
-                    y += lineHeight;
-                    lines++;
-                } else {
-                    skipToNextLine = true;
-
-                    if(ellipsis && this.data.length)
-                        this.data[this.data.length - 1].text = trim(this.data[this.data.length - 1].text, ".,; ") + "...";
-
-                    continue;
-                }
-            }
-
-            if(y > maxY || (numLines && lines > numLines)) {
-                if(pageBreak) {
-                    this.data.push({ pageBreak: true });
-                    this.pages++;
-                    x = margin.left;
-                    y = margin.top;
-                    lines = 0;
-                } else if(ellipsis && this.data.length) {
-                    this.data[this.data.length - 1].text = trim(this.data[this.data.length - 1].text, ".,; ") + "...";
-                    break;
-                }
-            }
-
-            token.x = x;
-            token.y = y;
-            this.data.push(token);
-
-            x += w;
+        for(let i = data.length - 1; i >= 0; i--) {
+            if(!data[i].text)
+                continue;
+            data[i].text = trim(data[i].text, ".,; ") + "...";
+            data[i].width = doc.getTextDimensions(data[i].text).w;
+            break;
         }
     }
 
-    this.finalY = y + lineHeight;
-    this.finalHeight = this.pages == 1
-        ? y - startY + lineHeight
-        : y - margin.top + lineHeight;
-
-    doc.restoreGraphicsState();
+    function getLineAndJustify(data, lineStart, textAlign, br) {
+        let line = data.slice(lineStart);
+        justifyLine(doc, line, margin.left, width, textAlign, br);
+    }
 
     this.draw = function () {
         doc.saveGraphicsState();
 
         for(let token of this.data) {
-            if(token.br)
-                continue;
-
             if(token.pageBreak) {
                 doc.addPage();
                 continue;
             }
 
+            if(token.h1) {
+                applyStyles(styles.h1);
+                continue;
+            }
+
+            if(token.h2) {
+                applyStyles(styles.h2);
+                continue;
+            }
+
+            if(token.p) {
+                applyStyles(); //revert to default
+                continue;
+            }
+
             if(token.bold) {
-                doc.setFont(undefined, "bold");
+                applyStyles({ bold: true });
                 continue;
             }
 
             if(token.bold === false) {
-                doc.setFont(undefined, "normal");
+                applyStyles({ bold: false });
                 continue;
             }
 
@@ -261,4 +479,7 @@ export function textBox(doc, text, { startY = 0, margin, width, baseline = "top"
 
         return this;
     };
+
+    //Measure only, this will give the caller the dimensions in order to draw something else below the text first
+    this.compile();
 }
